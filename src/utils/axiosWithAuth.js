a574c65ab0d4aca234e9c526576a6937f625e31f
Jordan <jordandevaney28@gmail.com>
2020-10-30 21:16:41 -0400

    change axios url

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,8 +4,8 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    // baseURL: "https://anywhere-fitness-app2.herokuapp.com/api/",
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://sample12342.herokuapp.com/api/",
+    //baseURL: "http://localhost:3000/api/",
     credentials: 'include',
     headers: {
       Authorization: token,
